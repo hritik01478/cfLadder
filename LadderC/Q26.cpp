d9ddacad661124ab6608e71a2b2dfba899e998f6
hritik01478 <hritikkumar01478@gmail.com>
2022-08-13 18:04:56 +0530

    Q26 - Q30 done in Ladder C

---
+++ LadderC/Q26.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repi(i, a, b) for (int i = (int)(a); i < (int)(b); i++)
+typedef long long int ll;
+ll mod = 1000000007;
+ll n;
+vector<ll> arr;
+
+// ll gcd(ll a, ll b)
+// {
+//     return b == 0 ? a : gcd(b, a % b);
+// }
+
+ll power(ll x, ll y)
+{
+    ll res = 1;
+    if (y == 0)
+        return 1;
+    while (y)
+    {
+        if ((y & 1LL))
+        {
+            res = (res * x) % mod;
+            y--;
+        }
+        y >>= 1LL;
+        x = (x * x) % mod;
+    }
+    return res;
+}
+
+// ll digits(ll n)
+// {
+//     ll ctr = 0;
+//     while (n)
+//     {
+//         ctr++;
+//         n /= 10;
+//     }
+//     return ctr;
+// }
+
+void solve_hritik()
+{
+    cin >> n;
+    vector<pair<ll, ll>> arr(n);
+    rep(i, n)
+    {
+        cin >> arr[i].first >> arr[i].second;
+    }
+    ll day = 1;
+    sort(arr.begin(), arr.end());
+    rep(i, n)
+    {
+        if (day > arr[i].second)
+        {
+            day = arr[i].first;
+        }
+        else
+        {
+            day = arr[i].second;
+        }
+    }
+    cout << day << endl;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve_hritik();
+    }
+}
